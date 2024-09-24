0c56149871403020823edeff2b4a09722570c97b
Andreswill <juniorpachecop321@gmail.com>
2024-09-24 15:54:08 -0500

    Inicializando proyecto de aprendizaje

---
+++ Statements/select.sql
@@ -0,0 +1,3 @@
+SELECT * FROM name_table; --nos devuelve o muestra toda la información de la tabla a consultar
+SELECT name FROM; --muestra solo los nombres de la columna a consultar
+SELECT id_user, name FROM; --muestra las dos columnas separadas por (,) a consultar
